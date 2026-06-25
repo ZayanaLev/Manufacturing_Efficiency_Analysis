087b7674b71dee05d4e601c632b5abb148c88384
Zayana Levchenkova <zayana.khamurova@seznam.cz>
2026-06-25 12:17:03 +0700

    Update data_analysis.sql
    Few Examples of SQL requests from PostgreSQL.

---
+++ 01_SQL/data_analysis.sql
@@ -0,0 +1,33 @@
+Ctrl+S
+
+--Total Downtime (with Join)
+  SELECT
+  lp.batch,
+  lp.product,
+  lp.operator,
+  td.total_downtime
+FROM line_productivity AS lp
+JOIN vw_total_downtime AS td
+  ON lp.batch = td.batch;
+
+Average Downtime by Product
+Select
+lp.product,
+ROUND(AVG(td.total_downtime)::numeric, 2) AS avg_downtime
+FROM line_productivity AS lp
+JOIN vw_total_downtime AS td
+  ON lp.batch = td.batch
+Group by lp.product
+Order by avg_downtime DESC;
+
+Top10 batches
+Select
+lp.batch,
+lp.product,
+lp.operator,
+td.total_downtime
+From line_productivity As lp
+Join vw_total_downtime As td
+On lp.batch=td.batch
+Order by total_downtime DESC
+LIMIT 10;
